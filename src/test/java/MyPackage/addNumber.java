b82ca391ed30c6e6f5b9ed58781423abb0098a8a
mustafizurrhmn50@gmail.com <mustafizurrhmn50@gmail.com>
2021-12-20 17:34:19 +0530

    Commit message

---
+++ src/test/java/MyPackage/addNumber.java
@@ -0,0 +1,19 @@
+package MyPackage;
+
+public class addNumber {
+
+	public static void main(String[] args) {
+		Demo();
+		
+	}
+	
+	public static void Demo()
+	{
+		
+        System.out.println("Hello Mustafizur");
+		System.out.println("How are You?");
+		System.out.println("Good Morning");
+
+	}
+
+}
