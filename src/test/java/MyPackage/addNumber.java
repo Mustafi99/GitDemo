c15b759c12e6823ddaf0bacb95f068a7739b6670
mustafizurrhmn50@gmail.com <mustafizurrhmn50@gmail.com>
2021-12-20 19:21:46 +0530

    Merging in Development Branch

--- src/test/java/MyPackage/addNumber.java
+++ src/test/java/MyPackage/addNumber.java
@@ -4,6 +4,7 @@ public class addNumber {
 
 	public static void main(String[] args) {
 		Demo();
+		Demo1();
 		
 	}
 	
@@ -14,6 +15,17 @@ public class addNumber {
 		System.out.println("How are You?");
 		System.out.println("Good Morning");
         System.out.println("Good Evening");
+        
+
+	}
+	
+	public static void Demo1()
+	{
+		
+        System.out.println("Hello Rahaman");
+		System.out.println("Whats Up?");
+		System.out.println("Have a nice day");
+        System.out.println("Take Care");
         
 
 	}
